e2ec69612ef67c7343a8ae0009850dd820db67db
Erik Martin <erik.martin@hitachivantara.com>
2021-02-16 17:09:22 -0500

    [DEVO-1242] Create a GitHub Action runner that works in the GKE cicd-cluster

---
+++ gke-org/env.sh
@@ -0,0 +1,5 @@
+export PROJECT_ID=changeme
+export CLUSTER_NAME=cicd-cluster
+export GITHUB_TOKEN=changeme-to-token-with-admin-org-perms
+export REPO_OWNER=changeme
+export REPO_URL=https://github.com/changme-org
